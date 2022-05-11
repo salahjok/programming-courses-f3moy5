c180e195541545485d9bbd4cc64968ac32e2789f
salahjok <salaheddineboualloul2@gmail.com>
2022-05-11 22:42:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-f3moy5
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-f3moy5)
---
+++ index.ts
@@ -0,0 +1,29 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let num1: number;
+let num2: number;
+let num3: number;
+let num4: number;
+let num5: number;
+num1 = 120;
+num2 = 400;
+num3 = 60;
+num4 = 20;
+num5 = num1 * num2 + num3 + num4;
+console.log(num5);
+console.log(num5 - num2 * num1);
+let f1: string;
+f1 = 'I study english';
+console.log(f1);
+let myNumber1: number;
+let myNumber2: number;
+let myNumber3: number;
+myNumber1 = 10;
+myNumber2 = 20;
+myNumber3 = myNumber1;
+myNumber1 = myNumber2;
+myNumber2 = myNumber3;
+console.log(10 + myNumber1);
+console.log(20 + myNumber2);
